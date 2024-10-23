63a835323170ccf7d48c486a4bee96f76f2dc363
felipeoliveiraescola <porfirio.silva@escola.pr.gov.br>
2024-10-23 08:42:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function mostra resultado()
   var numero1=10;
   var numero2=15;
   var resultado=numero1+numero
-    document.get element by idc'resultado')
+    document.getelementbyid("resultado')
   innerhtml=resultado
 }
